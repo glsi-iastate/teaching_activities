cd00835fefcb7d14ed9e0a47b8ac10f4773b735b
Alex Cecil <75514611+ajcecil@users.noreply.github.com>
2026-09-11 13:18:23 -0500

    Adjust arrowDx values for chart vertices

--- docs/js/script.js
+++ docs/js/script.js
@@ -150,7 +150,7 @@ const layout = {
       x: 0.35,
       y: 0.50,
       angle: 0,
-      arrowDx: -50,
+      arrowDx: -60,
       arrowDy: 0
     }),
 
@@ -160,7 +160,7 @@ const layout = {
       x: 0.65,
       y: 0.50,
       angle: 0,
-      arrowDx: 40,
+      arrowDx: 52,
       arrowDy: -30
     }),
 
@@ -170,7 +170,7 @@ const layout = {
       x: 0.50,
       y: -0.06,
       angle: 0,
-      arrowDx: 40,
+      arrowDx: 52,
       arrowDy: 30
     }),
   ],
